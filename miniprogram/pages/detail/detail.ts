d5fd7ceb88c90a175facd7645c31fb772ad3a55c
Amy <amy.su0123@gmail.com>
2019-04-25 11:47:45 +0800

    update style

--- miniprogram/pages/detail/detail.ts
+++ miniprogram/pages/detail/detail.ts
@@ -1,5 +1,8 @@
 Page({
     data: {
-        
-    }
-})
+        pid:0,          // 小程序详情id
+    },
+    onLoad(options: any){
+        this.data.pid = options.id;
+    },
+});
---
+++ miniprogram/utils/baseUrl.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.baseUrl = 'http://localhost:3000/';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmFzZVVybC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImJhc2VVcmwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBYSxRQUFBLE9BQU8sR0FBRyx3QkFBd0IsQ0FBQSIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjb25zdCBiYXNlVXJsID0gJ2h0dHA6Ly9sb2NhbGhvc3Q6MzAwMC8nIl19
