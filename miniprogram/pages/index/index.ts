c5b64965135cf8de75b68db6672834f9e0c779cb
Amy <amy.su0123@gmail.com>
2019-04-25 16:45:45 +0800

    add 接口获取数据[产品列表]

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -2,60 +2,28 @@
 //获取应用实例
 // import { IMyApp } from '../../app'
 // const app = getApp<IMyApp>()
-
+import {baseUrl} from '../../utils/baseUrl'
 Page({
   data: {
     tabIndex: 0,        // head-tab默认选中下标0的项
-    icons:[             // 图标集合
-      {
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXbaR2BLk7Ioig5tUOH14XAOxGpLdDf591oRbrtyLIZ01DHONmBA',
-        title: '小程序1',
-        id: 1
-      },
-      {
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3Qx5GWpzEnfTx7YQWX4rEjENki6qHoic0uyknmY5W20ph81cYJw',
-        title: '小程序2',
-        id: 2
-      },
-      {
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlwI94rffULFyk_e8kWpkYoQpleguQlM14Ngt4HFIuzaJhIcr5',
-        title: '小程序3',
-        id: 3
-      },
-      {
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT01wMSlpO1wnTD6AyWjgAC7dyf_bbGEEqmLYV1G8ZbhbV2nRdo',
-        title: '小程序4',
-        id: 4
-      },
-      {
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFs2v7xXGDOcA5WHmiKT93QBaijGchUSE_moNFRojMKo0VTZpliQ',
-        title: '小程序5',
-        id: 5
-      },
-      {
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlrLRUc7fYs-8cdXqLPaHsTfB20WqKeI9ISkUjfGcA-15pJyR26A',
-        title: '小程序6',
-        id: 6
-      },
-      {
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb2qVoEurRPrAxDWeiYA7lpQHrpdVZTi1dpMu2fCLP74EwUJf0',
-        title: '小程序7',
-        id: 7
-      },
-      {
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT00O5VISy9pDqnd0Pgzj7MvAOqQwjknXJWAPnYRsL7odlh3mGz',
-        title: '小程序8',
-        id: 8
-      },
-      {
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi8c9nlAj_JIpTgxsE4Mp3vy0lYxzsq2XY-4-Vrydf3ecmovNT',
-        title: '小程序9',
-        id: 9
-      },
-    ],
+    icons: [],
   },
   onLoad() {
-    
+    let that = this;
+    // 获取列表数据
+    wx.request({
+      url: baseUrl + '/product/findAll',
+      data: {},
+      success(res) {
+        let result: any = res.data || {};
+        that.setData!({
+          icons: result.data
+        })
+      },
+      fail(res) {
+        console.log('error:', res)
+      }
+    })
   },
 
   // 切换head tab
